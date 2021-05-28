86aaf71f70d586fa81403d642fa482151e6e391b
Paúl Ganán <paulisrael95@gmail.com>
2021-05-27 21:28:38 -0500

    start project

---
+++ src/components/home/home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <nav>
+                <li>home</li>
+            </nav>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <span>filter</span>
+                <span>filter</span>
+            </div>
+            <section>
+<div className="card">
+    card
+</div>
+            </section>
+        </div>
+    )
+}
